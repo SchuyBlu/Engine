bb67d27960076d73fe99771a0c857a08a424fa42
Schuy <schuylertk@gmail.com>
2024-10-21 09:34:25 -0600

    Added standard vector operations
    - Add
    - Sub
    - Dot
    - Cross
    - Normalize

--- src/Vectors/Vec3D.cs
+++ src/Vectors/Vec3D.cs
@@ -4,7 +4,7 @@ namespace Engine.Vectors;
 
 public class Vec3D
 {
-    double x, y, z;
+    private double x, y, z;
 
     public Vec3D(double x, double y, double z)
     {
@@ -13,14 +13,82 @@ public class Vec3D
         this.z = z;
     }
 
+	public double GetX()
+	{
+		return this.x;
+	}
+
+	public double GetY()
+	{
+		return this.y;
+	}
+
+	public double GetZ()
+	{
+		return this.z;
+	}
+
     public override string ToString()
     {
         return $"X: {this.x}, Y: {this.y}, Z: {this.z}";
     }
 
-    public double GetMagnitude()
+    public double GetLength()
     {
         double sum = Math.Pow(this.x, 2) + Math.Pow(this.y, 2) + Math.Pow(this.z, 2);
         return Math.Sqrt(sum);
     }
+
+	public Vec3D Normalize()
+	{
+		double x = this.x / this.GetLength();
+		double y = this.y / this.GetLength();
+		double z = this.z / this.GetLength();
+
+		return new Vec3D(x, y, z);
+	}
+
+	public Vec3D Add(Vec3D other)
+	{
+		double x = this.x + other.GetX();
+		double y = this.y + other.GetY();
+		double z = this.z + other.GetZ();
+
+		return new Vec3D(x, y, z);
+	}
+
+	public Vec3D Sub(Vec3D other)
+	{
+		double x = this.x - other.GetX();
+		double y = this.y - other.GetY();
+		double z = this.z - other.GetZ();
+
+		return new Vec3D(x, y, z);
+	}
+
+	public double Dot(Vec3D other)
+	{
+		double rx = this.x * other.GetX();
+		double ry = this.y * other.GetY();
+		double rz = this.z * other.GetZ();
+
+		return rx + ry + rz;
+	}
+
+	public Vec3D Cross(Vec3D other)
+	{
+        //         |  i   j   k |
+        // A x B = | ax  ay  az |
+        //         | bx  by  bz |
+        //     | ay az |     | ax az |     | ax ay |
+        // = i | by bz | - j | bx bz | + k | bx by |
+		//
+		// Get determinant via ad - bc
+
+		double i = (this.y * other.GetZ()) - (this.z * other.GetY());
+		double j = -((this.x * other.GetZ()) - (this.z * other.GetX()));
+		double k = (this.x * other.GetY()) - (this.y * other.GetX());
+
+		return new Vec3D(i, j, k);
+	}
 }
--- test/VectorsTests.cs
+++ test/VectorsTests.cs
@@ -5,11 +5,80 @@ namespace Engine.UnitTests.Vectors;
 public class Vec3DTests
 {
     [TestMethod]
-    public void Should_Give_Correct_Magnitude()
+    public void Should_Give_Correct_Length()
     {
         Vec3D vec = new(2.123, 8.182, 9.128);
 
-        decimal difference = (decimal)(vec.GetMagnitude() - 12.4407651292);
+        decimal difference = (decimal)(vec.GetLength() - 12.4407651292);
         Assert.IsTrue(difference < 0.00001M);
     }
+
+	[TestMethod]
+	public void Should_Normalize_Correctly()
+	{
+		Vec3D vec1 = new(2.0, 2.0, 1.0);
+		
+		Vec3D result1 = vec1.Normalize();
+
+		Assert.IsTrue(result1.GetX() - 0.666666666 < 0.00001D);
+		Assert.IsTrue(result1.GetY() - 0.666666666 < 0.00001D);
+		Assert.IsTrue(result1.GetZ() - 0.333333333 < 0.00001D);
+	}
+
+	[TestMethod]
+	public void Should_Add_Vectors_Correctly()
+	{
+		Vec3D vec1 = new(2.0, 3.0, 5.0);
+		Vec3D vec2 = new(1.0, 2.0, 7.0);
+
+		Vec3D result = vec1.Add(vec2); // (3.0, 5.0, 12.0)
+
+		Assert.AreEqual(3.0, result.GetX());
+		Assert.AreEqual(5.0, result.GetY());
+		Assert.AreEqual(12.0, result.GetZ());
+	}
+
+	[TestMethod]
+	public void Should_Sub_Vectors_Correctly()
+	{
+		Vec3D vec1 = new(2.0, 3.0, 5.0);
+		Vec3D vec2 = new(1.0, 2.0, 7.0);
+
+		Vec3D result = vec1.Sub(vec2);
+
+		Assert.AreEqual(1.0, result.GetX());
+		Assert.AreEqual(1.0, result.GetY());
+		Assert.AreEqual(-2.0, result.GetZ());
+	}
+
+	[TestMethod]
+	public void Should_Dot_Product_Correctly()
+	{
+		Vec3D vec1 = new(2.0, 3.0, 5.0);
+		Vec3D vec2 = new(1.0, 2.0, 7.0);
+
+		double result = vec1.Dot(vec2);
+		/*
+		 * 2.0 * 1.0 = 2.0
+		 * 3.0 * 2.0 = 6.0
+		 * 5.0 * 7.0 = 35.0
+		 * ----------------
+		 *             43.0
+		 */
+
+		Assert.AreEqual(43.0, result);
+	}
+
+	[TestMethod]
+	public void Should_Cross_Product_Correctly()
+	{
+		Vec3D vec1 = new(2.0, 3.0, 5.0);
+		Vec3D vec2 = new(1.0, 2.0, 7.0);
+
+		Vec3D result = vec1.Cross(vec2);
+
+		Assert.AreEqual(11.0, result.GetX());
+		Assert.AreEqual(-9.0, result.GetY());
+		Assert.AreEqual(1.0, result.GetZ());
+	}
 }
